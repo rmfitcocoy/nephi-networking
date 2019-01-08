4348ad992b94b03289bf6d93ef911ed9bd8c187e
Francisco Abayon <franciscoabayon@yahoo.com>
2019-01-08 23:09:04 +0800

    add bootstrap templates and assets
    https://uicookies.com/free-bootstrap-ecommerce-website-template/
    
    https://colorlib.com/preview/theme/cozastore/#

---
+++ !important/docs/documentation.php
@@ -0,0 +1,10 @@
+Themes Options:
+https://colorlib.com/preview/theme/essence/index.html
+https://colorlib.com/etc/coloshop/index.html#
+https://colorlib.com/etc/fashe/index.html#
+https://colorlib.com/preview/theme/cozastore/#
+https://colorlib.com/preview/theme/shop/
+UIKit Bootstrap Eccomerce
+
+Chosen Layout:
+cozastore
---
+++ zerhuel_app/logs/log-2019-01-08.php
@@ -0,0 +1,60 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2019-01-08 20:20:46 --> Config Class Initialized
+INFO - 2019-01-08 20:20:46 --> Hooks Class Initialized
+DEBUG - 2019-01-08 20:20:46 --> UTF-8 Support Enabled
+INFO - 2019-01-08 20:20:46 --> Utf8 Class Initialized
+INFO - 2019-01-08 20:20:46 --> URI Class Initialized
+DEBUG - 2019-01-08 20:20:47 --> No URI present. Default controller set.
+INFO - 2019-01-08 20:20:47 --> Router Class Initialized
+INFO - 2019-01-08 20:20:47 --> Output Class Initialized
+INFO - 2019-01-08 20:20:47 --> Security Class Initialized
+DEBUG - 2019-01-08 20:20:47 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-01-08 20:20:47 --> Input Class Initialized
+INFO - 2019-01-08 20:20:47 --> Language Class Initialized
+INFO - 2019-01-08 20:20:47 --> Loader Class Initialized
+DEBUG - 2019-01-08 20:20:47 --> Config file loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\config/development\maintenance_config.php
+INFO - 2019-01-08 20:20:47 --> Helper loaded: storehours_helper
+INFO - 2019-01-08 20:20:47 --> Email Class Initialized
+DEBUG - 2019-01-08 20:20:47 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2019-01-08 20:20:48 --> Session: Class initialized using 'files' driver.
+INFO - 2019-01-08 20:20:48 --> Controller Class Initialized
+INFO - 2019-01-08 20:20:48 --> Development:xxxxxxxxxxx-0.0.1 is executing maintenance_hook file in C:\xampp\htdocs\nephi-networking\zerhuel_app\hooks by DESKTOP-225VK9J with the unit name Cocoy
+.
+DEBUG - 2019-01-08 20:20:48 --> While Development:xxxxxxxxxxx-0.0.1 is in maintenance mode, it was successfully  access by DESKTOP-225VK9J using the unit name Cocoy
+.
+INFO - 2019-01-08 20:20:48 --> Development:xxxxxxxxxxx-0.0.1 is executing ssl_hook file in C:\xampp\htdocs\nephi-networking\zerhuel_app\hooks by DESKTOP-225VK9J with the unit name Cocoy
+.
+INFO - 2019-01-08 20:20:48 --> Helper loaded: url_helper
+DEBUG - 2019-01-08 20:20:48 --> Development:xxxxxxxxxxx-0.0.1DESKTOP-225VK9J with the unit name Cocoy
+ is accessing with SSL Request.
+DEBUG - 2019-01-08 20:20:48 --> Encryption: Auto-configured driver 'openssl'.
+INFO - 2019-01-08 20:20:48 --> Encryption: OpenSSL initialized with method AES-128-CBC.
+INFO - 2019-01-08 20:20:48 --> Encryption Class Initialized
+INFO - 2019-01-08 20:20:48 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\welcome_message.php
+DEBUG - 2019-01-08 20:20:48 --> Config file loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\config/development\develbar.php
+INFO - 2019-01-08 20:20:48 --> Helper loaded: utility_helper
+INFO - 2019-01-08 20:20:48 --> Helper loaded: language_helper
+INFO - 2019-01-08 20:20:48 --> Helper loaded: text_helper
+INFO - 2019-01-08 20:20:48 --> Language file loaded: language/english/develbar_lang.php
+DEBUG - 2019-01-08 20:20:48 --> DevelBar Class Initialized !
+INFO - 2019-01-08 20:20:48 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/benchmarks.php
+INFO - 2019-01-08 20:20:48 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/memory_usage.php
+INFO - 2019-01-08 20:20:48 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/request.php
+INFO - 2019-01-08 20:20:48 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/database.php
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/hooks.php
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/ajax.php
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/libraries.php
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/helpers.php
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/views.php
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/config.php
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/session.php
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/models.php
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_public\/assets/develbar/css/develbar.css
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_public\/assets/develbar/js/develbar.js
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_app\views\utilities/develbar/develbar.php
+INFO - 2019-01-08 20:20:49 --> File loaded: C:\xampp\htdocs\nephi-networking\zerhuel_public\/assets/develbar/js/ajax.js
+INFO - 2019-01-08 20:20:49 --> Final output sent to browser
+DEBUG - 2019-01-08 20:20:49 --> Total execution time: 3.1900
+INFO - 2019-01-08 20:20:49 --> Development:xxxxxxxxxxx-0.0.1 maintenance_hook file in C:\xampp\htdocs\nephi-networking\zerhuel_app\hook was executed completely.
+INFO - 2019-01-08 20:20:49 --> Development:xxxxxxxxxxx-0.0.1 ssl_hook file in C:\xampp\htdocs\nephi-networking\zerhuel_app\hook was executed completely.
